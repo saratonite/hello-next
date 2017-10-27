e7c6c635337f415a698e7bdbe9a196b87f10f494
Sarath <sarath.hacks@gmail.com>
2017-10-27 13:27:29 +0530

    :pig: Dynamic content from query string :bulb:

--- pages/index.js
+++ pages/index.js
@@ -2,9 +2,20 @@ import Link from 'next/link'
 import Hello from '../components/hello'
 import Layout from '../components/layout'
 
+const PostLink = (props) => (
+    <li>
+        <Link href={`/post?title=${props.title}`}><a>{props.title}</a></Link>
+    </li>
+)
+
 export default () =>(
     <Layout>
     
         <Hello/>
+        <ul>
+            <PostLink title="hello-world"></PostLink>
+            <PostLink title="Next.js"></PostLink>
+
+        </ul>
     </Layout>
 )
